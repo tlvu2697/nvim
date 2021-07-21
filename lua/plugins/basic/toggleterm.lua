87ccfc0f0651395316b2bb5dfc4cae36f3f8dc00
Le-Vu Tran <vu.tran@employmenthero.com>
2021-07-21 14:57:08 +0700

    Update toggleterm

--- lua/plugins/basic/toggleterm.lua
+++ lua/plugins/basic/toggleterm.lua
@@ -1,9 +1,16 @@
 ----------------------------------------------------
 -- https://github.com/akinsho/nvim-toggleterm.lua
 ----------------------------------------------------
-require('toggleterm').setup{}
+require("toggleterm").setup{
+  size = 12,
+  open_mapping = [[<c-\>]],
+  shade_terminals = false,
+  persist_size = true,
+  direction = 'horizontal',
+  shell = vim.o.shell,
+}
 
 cmd([[
-  command! -count=1 TermCIBranch lua require'toggleterm'.exec('source ~/.git_ci && cib && exit', <count>, 12)
-  command! -count=1 TermGitCreatePR lua require'toggleterm'.exec('source ~/.git_ci && create pr && exit', <count>, 12)
+  command! -count=1 TermCIBranch lua require'toggleterm'.exec('cib && exit', <count>, 12)
+  command! -count=1 TermGitCreatePR lua require'toggleterm'.exec('create pr && exit', <count>, 12)
 ]])
