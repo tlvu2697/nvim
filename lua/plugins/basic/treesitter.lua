10859e59349d91f971e74c4b956e3009c67d3929
Le-Vu Tran <vu.tran@employmenthero.com>
2021-09-06 22:55:44 +0700

    Update config

--- lua/plugins/basic/treesitter.lua
+++ lua/plugins/basic/treesitter.lua
@@ -22,7 +22,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  indent = { enable = false },
+  indent = { enable = true },
   autotag = { enable = true },
   autopairs = { enable = true },
   matchup = { enable = true },
