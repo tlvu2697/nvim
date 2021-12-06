44070c75de9b85e3a47ebb13bba25124a917ba96
Le-Vu Tran <vu.tran@employmenthero.com>
2021-12-06 16:32:30 +0700

    Enable indent for Ruby

--- lua/plugins/basic/treesitter.lua
+++ lua/plugins/basic/treesitter.lua
@@ -22,10 +22,7 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-  indent = {
-    enable = true,
-    disable = { 'ruby' },
-  },
+  indent = { enable = true },
   autotag = { enable = true },
   autopairs = { enable = true },
   matchup = { enable = true },
