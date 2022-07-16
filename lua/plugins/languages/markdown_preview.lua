64a840f96a5db300d6a708c569e921c255866607
Le-Vu Tran <vu.tran@employmenthero.com>
2022-07-16 20:06:22 +0700

    Update config for markdown preview

--- lua/plugins/languages/markdown_preview.lua
+++ lua/plugins/languages/markdown_preview.lua
@@ -1,7 +1,4 @@
 ----------------------------------------------------
 -- https://github.com/iamcco/markdown-preview.nvim
 ----------------------------------------------------
-g.mkdp_auto_start = 0
-g.mkdp_auto_close = 0
-g.mkdp_refresh_slow = 1
 map("n", "<space>fp", ":MarkdownPreview<CR>")
