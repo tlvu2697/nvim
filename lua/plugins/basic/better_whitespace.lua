c52c726510b839f6ba0ba2ee2192bd1c5f79f0b1
Le-Vu Tran <vu.tran@employmenthero.com>
2022-07-26 13:31:41 +0700

    Update better whitespace config

--- lua/plugins/basic/better_whitespace.lua
+++ lua/plugins/basic/better_whitespace.lua
@@ -5,3 +5,4 @@ g.strip_whitespace_on_save = 1
 g.strip_whitespace_confirm = 0
 g.better_whitespace_enabled = 1
 g.strip_max_file_size = 0
+g.better_whitespace_filetypes_blacklist = { "diff", "git", "gitcommit", "unite", "qf", "help", "fugitive" }
