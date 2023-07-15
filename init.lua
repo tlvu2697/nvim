4d2b358b528011c57c41e1885ef01ead8fe5603f
Le-Vu Tran <vu.tran@employmenthero.com>
2023-07-15 13:34:25 +0700

    Update config

--- init.lua
+++ init.lua
@@ -1,5 +1,28 @@
 local start_time = vim.fn.reltime()
 
+for _, plugin in pairs({
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "logipat",
+  "rrhelper",
+  "spellfile_plugin",
+  "matchit",
+}) do
+  vim.g["loaded_" .. plugin] = 1
+end
+
 vim.loader.enable()
 require("plugins")
 
