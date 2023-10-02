5ec25b7cf7bbe9458b725f24907201e4430c4eb8
Le-Vu Tran <vu.tran@employmenthero.com>
2023-10-02 09:50:21 +0700

    Update config

--- lua/plugins/basic/indent_blankline.lua
+++ lua/plugins/basic/indent_blankline.lua
@@ -1,9 +1,7 @@
 ----------------------------------------------------
 -- https://github.com/lukas-reineke/indent-blankline.nvim
 ----------------------------------------------------
-require("ibl").setup({
-  char = "¦",
-  indent_blankline_use_treesitter = true,
-  show_current_context = true,
-  show_current_context_start = false,
-})
+require("ibl").setup {
+  indent = { char = "¦" },
+  scope = { enabled = true },
+}
