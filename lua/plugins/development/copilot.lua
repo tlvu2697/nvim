50326bd1b0b071e45bcf6195b77f4ffd90731519
Le-Vu Tran <vu.tran@employmenthero.com>
2024-05-16 18:39:38 +0700

    Update config

--- lua/plugins/development/copilot.lua
+++ lua/plugins/development/copilot.lua
@@ -8,7 +8,8 @@ return {
       inoremap <silent><script><expr> <C-\> copilot#Accept("\<CR>")
       inoremap <silent> <C-j> <Plug>(copilot-next)
       inoremap <silent> <C-k> <Plug>(copilot-previous)
-      let g:copilot_no_tab_map = v:true
     ]])
+
+    g.copilot_no_tab_map = true
   end,
 }
