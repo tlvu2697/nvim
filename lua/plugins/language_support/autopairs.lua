ccd97c3b44b7eb3138b203689fb9e181891aad22
Le-Vu Tran <vu.tran@employmenthero.com>
2024-06-04 11:47:37 +0700

    Update autopairs

--- lua/plugins/language_support/autopairs.lua
+++ lua/plugins/language_support/autopairs.lua
@@ -6,22 +6,20 @@ return {
   "windwp/nvim-autopairs",
   init = function()
     g.completion_confirm_key = ""
-  end,
-  config = function()
     local npairs = require("nvim-autopairs")
     local Rule = require("nvim-autopairs.rule")
     npairs.setup({ check_ts = true, map_cr = false })
 
     -- Confirm Completion with coc.nvim
     _G.MUtils.completion_confirm = function()
-      if fn["coc#pum#visible"]() then
-        fn["coc#pum#close"]()
+      if fn["coc#pum#visible"]() ~= 0 then
+        return fn["coc#pum#stop"]() .. npairs.autopairs_cr()
       end
 
       return npairs.autopairs_cr()
     end
 
-    map("i", "<CR>", "v:lua._G.MUtils.completion_confirm()", { expr = true, noremap = true })
+    map("i", "<CR>", "v:lua._G.MUtils.completion_confirm()", { expr = true, noremap = true, silent = true })
 
     npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
     npairs.add_rules({
