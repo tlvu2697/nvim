f4e37a753681b336ade8629173be620874608c6b
Le-Vu Tran <vu.tran@employmenthero.com>
2024-06-05 20:38:38 +0700

    Annoying shit

--- lua/plugins/development/copilot.lua
+++ lua/plugins/development/copilot.lua
@@ -1,15 +1,15 @@
 ----------------------------------------------------
 -- https://github.com/github/copilot.vim
 ----------------------------------------------------
+g.copilot_no_tab_map = true
+
 return {
   "github/copilot.vim",
-  config = function()
+  init = function()
     cmd([[
       inoremap <silent><script><expr> <C-\> copilot#Accept("\<CR>")
       inoremap <silent> <C-j> <Plug>(copilot-next)
       inoremap <silent> <C-k> <Plug>(copilot-previous)
     ]])
-
-    g.copilot_no_tab_map = true
   end,
 }
