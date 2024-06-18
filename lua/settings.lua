b6a7f0f5879a434c7a5ec0fb76cb23168aaec3d5
Le-Vu Tran <vu.tran@employmenthero.com>
2024-06-18 07:00:56 +0700

    Disable mouse

--- lua/settings.lua
+++ lua/settings.lua
@@ -36,7 +36,7 @@ opt.updatetime = 100
 opt.signcolumn = "yes"
 opt.shortmess:append({ c = true })
 opt.switchbuf:append({ "useopen" })
-opt.mouse = ""
+vim.o.mouse = ""
 ----------------------------------------------------
 -- SECTION: Theme
 ----------------------------------------------------
