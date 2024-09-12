b85fd91d7dc8001626d85fb34b3d9de3f0873efe
Le-Vu Tran <vu.tran@employmenthero.com>
2024-09-12 18:08:57 +0700

    Correct treesitter config

--- lua/plugins/language_support/treesitter.lua
+++ lua/plugins/language_support/treesitter.lua
@@ -12,61 +12,63 @@ return {
     "windwp/nvim-ts-autotag",
   },
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "dockerfile",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "proto",
-      "python",
-      "regex",
-      "ruby",
-      "sql",
-      "typescript",
-      "yaml",
-    },
-    highlight = {
-      enable = true,
-      disable = { "vimdoc" },
-      additional_vim_regex_highlighting = false,
-    },
-    indent = {
-      enable = true,
-      disable = { "ruby" },
-    },
-    autotag = { enable = true },
-    autopairs = { enable = true },
-    matchup = { enable = true },
-    textobjects = {
-      select = {
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = {
+        "bash",
+        "dockerfile",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "proto",
+        "python",
+        "regex",
+        "ruby",
+        "sql",
+        "typescript",
+        "yaml",
+      },
+      highlight = {
         enable = true,
-        lookahead = true,
-        keymaps = {
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["ab"] = "@block.outer",
-          ["ib"] = "@block.inner",
+        disable = { "vimdoc" },
+        additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+      autotag = { enable = true },
+      autopairs = { enable = true },
+      matchup = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = false,
+          goto_next_start = {
+            ["]]"] = "@function.outer",
+          },
+          goto_previous_start = {
+            ["[["] = "@function.outer",
+          },
         },
       },
-      move = {
-        enable = true,
-        set_jumps = false,
-        goto_next_start = {
-          ["]]"] = "@function.outer",
-        },
-        goto_previous_start = {
-          ["[["] = "@function.outer",
-        },
-      },
-    },
-  },
+    })
+  end,
   init = function()
     g.matchup_matchparen_offscreen = { method = "none" }
   end,
