a1c74b38551ab6ca469ba4e55ea8ca0db705520f
Le-Vu Tran <vu.tran@employmenthero.com>
2025-02-24 15:36:20 +0700

    Update config

--- lua/plugins/basic/vimux.lua
+++ lua/plugins/basic/vimux.lua
@@ -5,8 +5,6 @@ return {
   "benmills/vimux",
   init = function()
     g.VimuxOrientation = "h"
-    g.VimuxHeight = "30"
-    g.VimuxOrientation = "v"
-    g.VimuxHeight = "30"
+    g.VimuxHeight = "30%"
   end,
 }
